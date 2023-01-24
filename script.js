e59e4cecf1b814c2805ac170e609c0b35e2a7b9b
Rahuboy <rahulramach3000@gmail.com>
2023-01-24 12:42:37 +0530

    Added updated site

---
+++ script.js
@@ -0,0 +1,101 @@
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const cursor = document.querySelector(".cursor");
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = "#E5B850";
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  cursor.style.top = x;
+  cursor.style.left = y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+
+
+
+
+function myFunction() {
+  let copyText = clipboard.getAttribute("value");
+  navigator.clipboard.writeText(copyText);
+  document.getElementById("email-heading").innerHTML = "Email Copied!";
+  setTimeout(() => {
+    document.getElementById("email-heading").innerHTML = "Email";
+  }, "2000")
+
+}
+
+
+// const observer = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('fade-in-animation');
+//     }
+//   });
+// });
+
+// observer.observe(document.querySelector('.section-two-text-one-wrapper'));
+// observer.observe(document.querySelector('.section-three-heading-wrapper'));
+// observer.observe(document.querySelector('.section-three-text-one-wrapper'));
+// observer.observe(document.querySelector('.section-three-text-two-wrapper'));
+// observer.observe(document.querySelector('.section-three-text-three-wrapper'));
+
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
